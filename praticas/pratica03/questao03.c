c75bc890acce4afb769ce5c29903e30b64214343
lauravi1 <143663477+lauravi1@users.noreply.github.com>
2023-10-11 23:31:34 +0000

    aula 11/10

---
+++ praticas/pratica03/questao03.c
@@ -0,0 +1,11 @@
+/*
+  Faça um programa em C que imprima um qualitativo associado a uma nota lida conforme a tabela: 1 = Ruim; 2 = Insuficiente; 3 = Suficiente, 4 = Bom, 5 = Ótimo, e imprima Nota inválida nos demais casos.
+  */
+
+#include <stdio.h>
+
+int main() {
+  if 
+  
+  return 0;
+}
